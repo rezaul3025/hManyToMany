f239c591c14559ad9b685d49ba7396688ff5bd7b
rezaul3025 <rezaul.km105@gmail.com>
2015-04-27 22:55:07 +0200

    init 1

--- src/test/java/demo/domain/service/StudentServiceTest.java
+++ src/test/java/demo/domain/service/StudentServiceTest.java
@@ -51,14 +51,14 @@ public class StudentServiceTest
 		s.setName("Tom");
 		s.setDept("Tom Dept");
 		Student std = stdService.createStudent(s);
-		//assertEquals("5",std.getStdId().toString());
+		assertEquals("1",std.getStdId().toString());
 	}
 	
 	@Test
 	public void getAllTest()
 	{
 		List<Student> allStudent = stdService.getAll();
-		//assertEquals(46, allStudent.size());
+		assertEquals(1, allStudent.size());
 		
 		Student student = allStudent.get(0);
 		
