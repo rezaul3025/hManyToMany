aaa735be549c87c09aa62a9deafe0d90ca465c67
rezaul3025 <rezaul.km105@gmail.com>
2015-05-01 12:14:43 +0200

    test change

--- src/main/java/demo/HManyToManyApplication.java
+++ src/main/java/demo/HManyToManyApplication.java
@@ -3,7 +3,7 @@ package demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class HManyToManyApplication {
 
     //public static void main(String[] args) {
